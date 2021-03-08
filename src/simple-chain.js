179cc452cf11a2157cfc6b442209b58b21fceee9
Dovoyan <yankavalenkau@gmail.com>
2021-03-08 22:05:40 +0300

    feat: add simp

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,34 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  "cep": [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.cep.length();
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.cep.push(value);
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (!Number.isInteger(position) || position < 1 || position > this.cep.length - 1) {
+      throw new Error();
+    } else {
+      this.cep.splice(position - 1, 1);
+      return this;
+    }
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.cep.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let y = '';
+    this.cep.forEach(cepe => {
+      y += '( ' + cepe + ' )~~'
+    });
+    this.cep = [];
+    return y.substring(0, y.length - 2);
+
   }
 };
 
